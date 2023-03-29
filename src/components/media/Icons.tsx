ed39216a1df77296a21f6d34cdb2c3da344108cf
nishu-murmu <nishumurmu017@gmail.com>
2023-03-29 13:14:41 +0530

    adding media

--- src/components/media/Icons.tsx
+++ src/components/media/Icons.tsx
@@ -324,11 +324,11 @@ export const LinkIcon = () => {
       viewBox='0 0 16 16'
     >
       <path
-        fill-rule='evenodd'
+        fillRule='evenodd'
         d='M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z'
       />
       <path
-        fill-rule='evenodd'
+        fillRule='evenodd'
         d='M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z'
       />
     </svg>
@@ -472,9 +472,25 @@ export const BookMarkCheckFillIcon = (props: IconsProps) => {
       viewBox='0 0 16 16'
     >
       <path
-        fill-rule='evenodd'
+        fillRule='evenodd'
         d='M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zm8.854-9.646a.5.5 0 0 0-.708-.708L7.5 7.793 6.354 6.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z'
       />
     </svg>
   )
+}
+
+export const WarningIcon = ({width, height, fillColor}: IconsProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      fill={fillColor}
+      className="bi bi-exclamation-triangle"
+      viewBox="0 0 16 16"
+    >
+      <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z" />
+      <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z" />
+    </svg>
+  );
 }
