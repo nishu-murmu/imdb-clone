5f5fff83fe5724be81e667857e5258f17906dccf
nishu-murmu <nishumurmu017@gmail.com>
2023-03-29 18:59:26 +0530

    adding on click add card logic

--- src/components/CommonComponents/MovieCard.tsx
+++ src/components/CommonComponents/MovieCard.tsx
@@ -1,41 +1,48 @@
-import { useContext } from "react"
-import { useSelector } from "react-redux"
-import { AuthContext } from "../../contexts/authContext"
-import { RootState } from "../../utils/types"
-import { CheckMarkIcon, PlusIcon } from "../media/Icons"
-import { BookMarkCheckImage, BookMarkImage } from "../media/Images"
+import { useContext } from "react";
+import { useSelector } from "react-redux";
+import { AuthContext } from "../../contexts/authContext";
+import { RootState } from "../../utils/types";
+import { Link } from "react-router-dom";
+import { CheckMarkIcon, PlusIcon } from "../media/Icons";
+import { BookMarkCheckImage, BookMarkImage } from "../media/Images";
 
 const MovieCard = (props: any) => {
-
-  const authContext = useContext(AuthContext)
-  const {currentUser} = authContext
-  const selectedList = useSelector((state:RootState) => state.hero.selectedItems)
+  const authContext = useContext(AuthContext);
+  const { currentUser } = authContext;
+  const selectedList = useSelector(
+    (state: RootState) => state.hero.selectedItems
+  );
 
   return (
     <div
       className="bg-emerald-300 mb-1 group relative w-[165px] h-[244px] "
       onClick={() => props.onBookmarkHandler()}
     >
-      <div className="absolute top-0 left-0 group-hover:cursor-pointer">
-        <BookMarkImage
-          fillColor={props.bgfillColor}
-          width={props.width}
-          height={props.height}
-        />
-      </div>
-      <div className="absolute top-1 left-[8px] mt-2 ml-.5 group-hover:cursor-pointer">
-        {currentUser &&selectedList?.includes(props.cardId) ? (
-          <CheckMarkIcon
-            height="25"
-            width="25"
-            fillColor={props.iconFillColor}
-          />
-        ) : (
-          <PlusIcon fillColor={props.iconFillColor} />
-        )}
-      </div>
+      <Link to={"/signin"}>
+        <div>
+          <div className="absolute top-0 left-0 group-hover:cursor-pointer">
+            <BookMarkImage
+              fillColor={props.bgfillColor}
+              width={props.width}
+              height={props.height}
+            />
+          </div>
+          <div className="absolute top-1 left-[8px] mt-2 ml-.5 group-hover:cursor-pointer">
+            {currentUser && selectedList?.includes(props.cardId) ? (
+              <CheckMarkIcon
+                height="25"
+                width="25"
+                fillColor={props.iconFillColor}
+              />
+            ) : (
+              <PlusIcon fillColor={props.iconFillColor} />
+            )}
+          </div>
+        </div>
+      </Link>
+
       <img src={props.imgUrl} alt="" />
     </div>
   );
-}
-export default MovieCard
+};
+export default MovieCard;
